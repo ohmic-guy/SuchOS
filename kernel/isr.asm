6c8c82d363848b0b10b2adcaa979438aa4f3e5d9
ohmic-guy <omkarankit2004@gmail.com>
2026-06-24 10:47:42 +0530

    refactor: remove redundant comments and reformat ISR/IRQ table for improved readability

--- kernel/isr.asm
+++ kernel/isr.asm
@@ -1,6 +1,5 @@
 [BITS 32]
 
-; ISR without CPU error code — push dummy 0
 %macro ISR_NOERR 1
 [global isr%1]
 isr%1:
@@ -9,7 +8,6 @@ isr%1:
     jmp isr_common
 %endmacro
 
-; ISR with CPU error code — already on stack
 %macro ISR_ERR 1
 [global isr%1]
 isr%1:
@@ -17,7 +15,6 @@ isr%1:
     jmp isr_common
 %endmacro
 
-; IRQ — mapped to INT (32 + N)
 %macro IRQ 2
 [global irq%1]
 irq%1:
@@ -26,21 +23,55 @@ irq%1:
     jmp irq_common
 %endmacro
 
-; CPU exceptions 0-31
-ISR_NOERR 0   ISR_NOERR 1   ISR_NOERR 2   ISR_NOERR 3
-ISR_NOERR 4   ISR_NOERR 5   ISR_NOERR 6   ISR_NOERR 7
-ISR_ERR   8   ISR_NOERR 9   ISR_ERR   10  ISR_ERR   11
-ISR_ERR   12  ISR_ERR   13  ISR_ERR   14  ISR_NOERR 15
-ISR_NOERR 16  ISR_ERR   17  ISR_NOERR 18  ISR_NOERR 19
-ISR_NOERR 20  ISR_NOERR 21  ISR_NOERR 22  ISR_NOERR 23
-ISR_NOERR 24  ISR_NOERR 25  ISR_NOERR 26  ISR_NOERR 27
-ISR_NOERR 28  ISR_NOERR 29  ISR_ERR   30  ISR_NOERR 31
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR   8
+ISR_NOERR 9
+ISR_ERR   10
+ISR_ERR   11
+ISR_ERR   12
+ISR_ERR   13
+ISR_ERR   14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_ERR   17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_NOERR 21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_NOERR 29
+ISR_ERR   30
+ISR_NOERR 31
 
-; Hardware IRQs 0-15 → INT 32-47
-IRQ  0, 32   IRQ  1, 33   IRQ  2, 34   IRQ  3, 35
-IRQ  4, 36   IRQ  5, 37   IRQ  6, 38   IRQ  7, 39
-IRQ  8, 40   IRQ  9, 41   IRQ 10, 42   IRQ 11, 43
-IRQ 12, 44   IRQ 13, 45   IRQ 14, 46   IRQ 15, 47
+IRQ  0, 32
+IRQ  1, 33
+IRQ  2, 34
+IRQ  3, 35
+IRQ  4, 36
+IRQ  5, 37
+IRQ  6, 38
+IRQ  7, 39
+IRQ  8, 40
+IRQ  9, 41
+IRQ 10, 42
+IRQ 11, 43
+IRQ 12, 44
+IRQ 13, 45
+IRQ 14, 46
+IRQ 15, 47
 
 [extern isr_handler]
 [extern irq_handler]
@@ -56,7 +87,7 @@ isr_common:
     mov es, ax
     mov fs, ax
     mov gs, ax
-    push esp            ; registers_t* regs
+    push esp
     call isr_handler
     pop eax
     pop gs
@@ -64,7 +95,7 @@ isr_common:
     pop es
     pop ds
     popa
-    add esp, 8          ; remove int_no + err_code
+    add esp, 8
     iret
 
 irq_common:
