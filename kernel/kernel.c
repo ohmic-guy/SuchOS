57b9f126dc1c66206f06afe1eda7b22486a6cb40
ohmic-guy <omkarankit2004@gmail.com>
2026-06-25 15:39:48 +0530

    feat: initialize GDT, PMM, and heap in kernel entry and bump version to v0.4

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,24 @@
 #include "vga.h"
+#include "gdt.h"
 #include "idt.h"
 #include "pic.h"
 #include "keyboard.h"
+#include "pmm.h"
+#include "heap.h"
+
+extern uint32_t kernel_end;
+
+#define HEAP_SIZE (4 * 1024 * 1024)
 
 void kernel_main(void) {
     terminal_init();
 
     terminal_setcolor(VGA_LIGHT_CYAN, VGA_BLACK);
-    terminal_write("[ SuchOS v0.3 ]\n\n");
+    terminal_write("[ SuchOS v0.4 ]\n\n");
 
     terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
-    terminal_write("VGA driver    ");
+    terminal_write("GDT           ");
+    gdt_init();
     terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
     terminal_write("[OK]\n");
 
@@ -21,22 +29,51 @@ void kernel_main(void) {
     terminal_write("[OK]\n");
 
     terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
-    terminal_write("PIC remap     ");
+    terminal_write("PIC           ");
     pic_init();
     terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
     terminal_write("[OK]\n");
 
     terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
-    terminal_write("Keyboard IRQ  ");
+    terminal_write("Keyboard      ");
     keyboard_init();
     terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
     terminal_write("[OK]\n");
+
+    terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
+    terminal_write("PMM           ");
+    pmm_init((uint32_t)&kernel_end);
+    terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
+    terminal_write("[OK] used=");
+    terminal_writedec(pmm_used_frames());
+    terminal_write(" frames\n");
+
+    terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
+    terminal_write("Heap          ");
+    uint32_t hstart = ((uint32_t)&kernel_end + 0xFFF) & ~0xFFFu;
+    heap_init(hstart, HEAP_SIZE);
+    terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
+    terminal_write("[OK] base=");
+    terminal_writehex(hstart);
+    terminal_write("\n");
+
+    terminal_setcolor(VGA_LIGHT_GREY, VGA_BLACK);
+    terminal_write("kmalloc test  ");
+    void* p = kmalloc(256);
+    if (p) {
+        terminal_setcolor(VGA_LIGHT_GREEN, VGA_BLACK);
+        terminal_write("[OK] ptr=");
+        terminal_writehex((uint32_t)p);
+        terminal_write("\n");
+    } else {
+        terminal_setcolor(VGA_LIGHT_RED, VGA_BLACK);
+        terminal_write("[FAIL]\n");
+    }
 
     terminal_setcolor(VGA_LIGHT_CYAN, VGA_BLACK);
     terminal_write("\n> ");
     terminal_setcolor(VGA_WHITE, VGA_BLACK);
 
     __asm__ volatile ("sti");
-
     for (;;) __asm__ volatile ("hlt");
 }
